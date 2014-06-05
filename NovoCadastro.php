bb10cf1d4ed89890891d9e3859b2819eda423e83
caiocsn <caiocsn@hotmail.com>
2014-06-05 14:49:34 -0300

    Phps Novo/Excluir adicionados

---
+++ NovoCadastro.php
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <?php
+        
+        //Setando valores das variáveis
+        
+        $nomeCompleto = $_POST ["nomeCompleto"];
+        $email = $_POST ["email"];
+        $curso = $_POST ["curso"];
+        $instituicao = $_POST ["instituicao"];
+        $cpf = $_POST ["cpf"];
+        $tel = $_POST ["telefone"];
+        $senha = $_POST ["senha"];
+        
+        // Estabelecendo conexão com o banco de dados        
+        include 'connectDB.php';
+        
+        // Inserindo dados na tabela
+        $query = "INSERT INTO Participante (nomeCompleto, email, curso, instituicao, cpf, 
+                                                telefone, senha)
+        VALUES ('$nomeCompleto', '$email', '$curso', '$instituicao' , '$cpf', '$tel',  
+					  '$senha')";
+                                          
+        mysql_query($query, $conexao);
+        
+        echo "Seu cadastro foi realizado com sucesso!<br>Agradecemos a atenção.";
+        
+        //Encerrando conexão com o banco de dados
+        mysql_close();
+        ?>
+    </body>
+</html>
